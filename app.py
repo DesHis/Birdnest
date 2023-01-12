a829d546f260e98b4da0f3a505857003dc978441
Altti Äyräs <asayra@utu.fi>
2023-01-13 01:25:49 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
 from flask import Flask, render_template
 
-app = Flask(__name__)
-@app.route('/')
-def index():
-    return render_template('/app/templates/index.html')
 
-if __name__ == '__main__':
-        app.run(debug=True, threaded=True, port=5000)
+while True:
+    try:
+        app = Flask(__name__)
+        @app.route('/')
+        def index():
+            return render_template('/app/templates/index.html')
+
+        if __name__ == '__main__':
+                app.run(debug=True, threaded=True, port=5000)
+    except:
+        print("epic fail")
