8fc7dca51fd0c074f77931daa8ffacda8ef28856
Altti Äyräs <asayra@utu.fi>
2023-01-13 01:38:19 +0200

    Update app.py

--- app.py
+++ app.py
@@ -7,4 +7,5 @@ def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-        app.run(debug=True, threaded=True, port=5000)
+        app.run(debug=True, threaded=True, port=5000,)
+        app.config['TEMPLATES_AUTO_RELOAD'] = True
