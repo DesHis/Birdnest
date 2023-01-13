fa61b76a20b7835277edcc643d137a7e47f69679
Altti Äyräs <asayra@utu.fi>
2023-01-13 14:42:52 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,12 @@
 from flask import Flask
 
-chungus=0
-
 app = Flask(__name__,)
 @app.route('/')
 def index():
-    global chungus
     page = open("templates/index.html", "r")
     content = page.read()
     page.close()
-    chungus+=1
-    return str(chungus)
+    return content
 
 
 if __name__ == '__main__':
