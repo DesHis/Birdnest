84b479aafcdd7de3b4945507571f44d5ce4afb03
Altti Äyräs <asayra@utu.fi>
2023-01-13 02:18:59 +0200

    the chunger

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask
 
 app = Flask(__name__,)
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return str(open("templates/index.html", "r").read())
 
 
 if __name__ == '__main__':
