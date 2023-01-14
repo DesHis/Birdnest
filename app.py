e9d1f1fdb29ecb546f38a67bb2886da4ae907b0d
Altti Äyräs <asayra@utu.fi>
2023-01-14 20:33:50 +0200

    migrate to pythonanywhere

--- app.py
+++ app.py
@@ -1,10 +1,4 @@
-from flask import Flask, render_template
-import subprocess
-import threading
-
-
-def runBirdnest():
-    subprocess.call(['python', 'birdnest.py'])
+from flask import Flask
 
 app = Flask(__name__,)
 @app.route('/')
@@ -12,11 +6,7 @@ def index():
     page = open("templates/index.html", "r")
     content = page.read()
     page.close()
-    #return render_template("index.html")
     return content
 
-
 if __name__ == '__main__':
-    thread=threading.Thread(target=runBirdnest)
-    thread.start()
     app.run(threaded=True, port=5000)
--- birdnest.py
+++ birdnest.py
@@ -1,14 +1,13 @@
 import requests
 import xml.etree.ElementTree as ElementTree
-import math 
-import json 
+import math
+import json
 import time
 
 
 violators=[]
-while True:
-    print("asdasd")
 
+while True:
     #get data from GET request and parse into tree
     response = requests.get("http://assignments.reaktor.com/birdnest/drones")
     try:
@@ -16,7 +15,6 @@ while True:
         capture=tree.find("capture")
     except:
         print("404 error")
-
 
     #if drone is within the circle, add its pilot data to violators list
     circleCenter=[250000, 250000]
@@ -32,13 +30,11 @@ while True:
                 pilotInfo=[violatorData["firstName"], violatorData["lastName"], violatorData["email"], violatorData["phoneNumber"], time.time(), distanceFromNest, serialNumber]
                 for v in violators: #check if pilot has violated before
                     if(v[6]==serialNumber):
-                        if(distanceFromNest<=v[5]): #update closest distance 
+                        if(distanceFromNest<=v[5]): #update closest distance
                             v[5]=distanceFromNest
                         AlreadyAdded=True
                 if(not AlreadyAdded):
                     violators.insert(0, pilotInfo)
-                    print("the violator")
-
             except:
                 print("404 error")
 
@@ -46,21 +42,20 @@ while True:
     #write violating pilot information to html file
     site1='<!DOCTYPE html><html lang="en"><head><title>Project Birdnest</title><script>function autoRefresh() {window.location = window.location.href;}setInterval("autoRefresh()", 2000);</script><meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate" /><meta http-equiv="Pragma" content="no-cache" /><meta http-equiv="Expires" content="0" /></head><body><main><h1>Project Birdnest</h1> <h2> Recent violators:</h2>'
     site2='</main></body></html>'
-    recentViolators=""  
+    recentViolators=""
 
     for pilotInfo in violators:
-
-        if(time.time()-pilotInfo[4]>600): #remove if older than 10 minutes 
-            violators.remove(pilotInfo) 
+        if(time.time()-pilotInfo[4]>600): #remove if older than 10 minutes
+            violators.remove(pilotInfo)
         recentViolators+=str(int(time.time()-pilotInfo[4]))+" SECONDS AGO "
         for i in range(4):
             recentViolators+=str(pilotInfo[i])+" "
         recentViolators+="closest distance: "+str(int(pilotInfo[5])/1000)+" meters "
         recentViolators+="<br>"
 
-    site = open("templates/index.html", "w")
+    site = open("/home/deshis/Birdnest/index.html", "w") #pythonanywhere doesnt like relative paths for some reason
     site.write(site1+recentViolators+site2)
     site.close
 
-    #since the XML data is updated every 2 seconds, we can sleep to avoid spamming the server with unnecessary requests 
+    #since the XML data is updated every 2 seconds, we can sleep to avoid spamming the server with unnecessary requests
     time.sleep(2)
